4c9254d22224f9d7869a229057eea617b0bbd0c7
xavaniarauan <167939622+xavaniarauan@users.noreply.github.com>
2024-04-24 15:50:10 +0800

    Add files via upload

---
+++ data/TERMINALBUS_2.js
@@ -0,0 +1,1 @@
+var json_TERMINALBUS_2 = {"type":"FeatureCollection","name":"TERMINALBUS_2","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"NAMOBJ":null,"REMARK":"Terminal Bus/Angkutan Kendaraan Lainnya"},"geometry":{"type":"Point","coordinates":[101.09894053000005,1.325393051000049]}}]}
